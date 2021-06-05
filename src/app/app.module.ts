e4f24b85ca67215cdafab5216f6f691dc884ba2b
BaoTuong <baopt@vnext.com.vn>
2021-06-05 21:44:14 +0700

    Base UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataTableComponent } from './shared/components/data-table/data-table.component';
+import { PaginationComponent } from './shared/components/pagination/pagination.component';
+import { ModelSearchComponent } from './shared/components/model-search/model-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent,
+    PaginationComponent,
+    ModelSearchComponent
   ],
   imports: [
     BrowserModule
